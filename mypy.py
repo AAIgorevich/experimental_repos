9cd46feb30177b681461b425e0f29725e91aaaaf
AAIgorevich <andrewarestotel@gmail.com>
2023-10-18 09:54:34 +0700

    feat: add new stuff

--- mypy.py
+++ mypy.py
@@ -1,9 +1,12 @@
 
 def calculated():
-    pass
+    x = 42
+    return x
 
 
 print("Write some think..")
 stuff = "U say: "
 word = f"{stuff} {input()}"
 print(word)
+print("Наверное у вас возникают вопросы?")
+print(f"Вот вам универсальный ответ: {calculated()}")
